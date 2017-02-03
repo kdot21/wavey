8823f05017b08f5215f3dd11e249352d653fc2e8
Kenneth Mancini <kennethjmancini@gmail.com>
2017-02-03 17:59:10 -0500

    Cleaned up JS file

--- scripts/main.js
+++ scripts/main.js
@@ -7,11 +7,10 @@ console.log('js!');
 
     var styles = [
       {
-        stylers: [
-          { hue: '#00c5cd' },
-          { saturation: -20 }
-        ]
-      }, {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [{color: '#00c5cd'}]
+      },{
           featureType: 'road',
           elementType: 'geometry',
           stylers: [
@@ -21,9 +20,7 @@ console.log('js!');
       }, {
           featureType: 'road',
           elementType: 'labels',
-          stylers: [
-            { visibility: 'off' }
-          ]
+          stylers: [{ visibility: 'off' }]
       }
     ];
 
@@ -50,57 +47,53 @@ $(document).ready(function() {
   console.log('jQuery!')
 
   // Weather Underground API for search capabilites
-  var WEATHER_UNDERGROUND_API = 'http://api.wunderground.com/api/b9a4b3dc52783dbb/conditions/q/';
+  var WUNDERGROUND_API = 'http://api.wunderground.com/api/b9a4b3dc52783dbb/conditions/q/';
 
-  var testCity = WEATHER_UNDERGROUND_API + 'CA/San_Francisco.json';
+  var testCity = WUNDERGROUND_API + 'CA/San_Francisco.json';
+
+  var searchVal = $('#search-bar').val();
 
   var forecastResults = $('#forecast-results');
-
-  function testCallWeatherAPI() {
-    $.get(testCity, function(whatever){
-      console.log(whatever);
-    })
-  }
-
-  testCallWeatherAPI();
-
-  function callWeatherAPI(city) {
-    if (city.length == 0) {
-      forecastResults.html('Please enter a city name into the search bar');
-    } else {
-      $.get(WEATHER_UNDERGROUND_API + city, function(forecastResults) {
-
-      });
-    }
-  }
 
   $('#search-button').on('click', function(e) {
     e.preventDefault();
 
-    callWeatherAPI($('#search-bar').val());
+    if (searchVal.length === 0) {
+      forecastResults.html('Please enter the name of a US city');
+    }
+
   });
 
-  // Appending array of surf spots
-  var surfSpots = [
-    'Point Judith \'The Lighthouse\'',
-    'Matunuck',
-    'Narragansett Beach',
-    'The K',
-    'Bonnet Point',
-    'Ruggles',
-    'First Beach',
-    'Second Beach',
-    'Monahan\'s Dock'
-  ];
+  function callWeatherAPI() {
+    $.get(testCity, function(weatherObject) {
+      console.log(weatherObject);
+    });
+  }
+
+  callWeatherAPI();
+
+  //// Appending array of surf spots
+  // var surfSpots = [
+  //   'Point Judith \'The Lighthouse\'',
+  //   'Matunuck',
+  //   'Narragansett Beach',
+  //   'The K',
+  //   'Bonnet Point',
+  //   'Ruggles',
+  //   'First Beach',
+  //   'Second Beach',
+  //   'Monahan\'s Dock'
+  // ];
 
   // $('#search-button').on('mouseover', function() {
 
   //   $.each(surfSpots, function(i,e) {
-  //     var spotsLi = "<li>";
-  //     spotsLi += e + "</li>";
+  //     var spotsLi = '<li>';
+  //     spotsLi += e + '</li>';
   //     $(spotsLi).appendTo('#surf-spots-ul');
 
   //   });
+
   // });
 
 });
